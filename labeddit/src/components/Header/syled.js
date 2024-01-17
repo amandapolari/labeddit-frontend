8ba732f8ec02e9fed0f63c1a9a35e01e6edeba25
Amanda <amandaoliveirapolari@gmail.com>
2024-01-17 11:33:57 -0400

    [raw] chakra-ui dependency removal

--- labeddit/src/components/Header/syled.js
+++ labeddit/src/components/Header/syled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { theme } from '../../styles/theme';
 
 export const HeaderStyled = styled.div`
     height: 10vh;
@@ -8,7 +7,6 @@ export const HeaderStyled = styled.div`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    background-color: ${theme.colors.gray['500']};
 `;
 
 export const ButtonLogin = styled.button`
@@ -17,7 +15,6 @@ export const ButtonLogin = styled.button`
     padding: 0.5em 1em;
     font-size: 1em;
     font-weight: 700;
-    color: ${theme.colors.blue['500']};
     cursor: pointer;
     transition: 0.3s;
     &:hover {
--- labeddit/src/index.js
+++ labeddit/src/index.js
@@ -3,15 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/* <ChakraProvider theme={theme}> */}
-            <GlobalStyle />
-            <App />
-        {/* </ChakraProvider> */}
+        <GlobalStyle />
+        <App />
     </React.StrictMode>
 );
